e5137464563f884e03a0c1a617fe972c281e7ea6
Kenny <kawhyte@gmail.com>
2020-01-07 15:30:42 -0800

    Added New JSON quote

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const quote = document.getElementById("quote");
+const season = document.getElementById("season");
+const photo = document.getElementById("photo");
 const author = document.getElementById("author");
 const getNewQuote = document.getElementById("get_quote");
 
-//getNewQuote.addEventListener('click', getMantra() )
+
 getNewQuote.addEventListener('click', getMantra)
 
 //Get quotes
 async function getMantra() {
-  const response = await fetch("quotes.json");
+  const response = await fetch("officequotes.json");
   const myJson = await response.json();
   const myJsonSize = JSON.stringify(myJson).length;
   console.log(myJsonSize);
-  num = Math.floor(Math.random() * Math.floor(100));
-  //console.log(num);
-
+  //let num = Math.floor(Math.random() * Math.floor(myJsonSize));
+  let num = Math.floor(Math.random() * 2) + 1  
+  //var randChoice = Math.floor(Math.random() * json["data"]["children"].length);
+  console.log(num);
+ console.log(myJson[num].text)
   if (myJson[num].text === null || myJson[num].from === null) {
     quote.textContent =
       "A hero is one who knows how to hang on for one minute longer.";
     author.textContent = "Norwegian proverb";
   } else {
-    quote.textContent = myJson[num].text;
+    quote.textContent = "'"+myJson[num].text+ "'";
     author.textContent = myJson[num].from;
+    season.textContent = myJson[num].season;
+
   }
 //   if ( a=== null || mantraData.text) {
 //     mantra.textContent = "Inhale love. Exhale gratitude.";
