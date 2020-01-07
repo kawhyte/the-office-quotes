2bf64fbf93f5508aa5d49feed0bcce33b7f3ef10
Kenny <kawhyte@gmail.com>
2020-01-07 08:28:05 -0800

    Added JS

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
+const quote = document.getElementById("joke");
+const author = document.getElementById("author");
+const getNewQuote = document.getElementById("get_joke");
 
+//getNewQuote.addEventListener('click', getMantra() )
+getNewQuote.addEventListener('click', getMantra)
 
-//Get focus
-// async function getMantra() {
-//   const response = await fetch("quotes.json");
-//   const myJson = await response.json();
-//   const myJsonSize = JSON.stringify(myJson).length;
-//   //console.log(myJsonSize);
-//   num = Math.floor(Math.random() * Math.floor(100));
-//   //console.log(num);
+//Get quotes
+async function getMantra() {
+  const response = await fetch("quotes.json");
+  const myJson = await response.json();
+  const myJsonSize = JSON.stringify(myJson).length;
+  console.log(myJsonSize);
+  num = Math.floor(Math.random() * Math.floor(100));
+  //console.log(num);
 
-//   if (myJson[num].text === null || myJson[num].from === null) {
-//     quote.textContent =
-//       "A hero is one who knows how to hang on for one minute longer.";
-//     author.textContent = "Norwegian proverb";
-//   } else {
-//     quote.textContent = myJson[num].text;
-//     author.textContent = myJson[num].from;
-//   }
-//   if ( === null || mantraData.text) {
+  if (myJson[num].text === null || myJson[num].from === null) {
+    quote.textContent =
+      "A hero is one who knows how to hang on for one minute longer.";
+    author.textContent = "Norwegian proverb";
+  } else {
+    quote.textContent = myJson[num].text;
+    author.textContent = myJson[num].from;
+  }
+//   if ( a=== null || mantraData.text) {
 //     mantra.textContent = "Inhale love. Exhale gratitude.";
 //   } else {
 
-//var json = JSON.parse(myJson);
+// var json = JSON.parse(myJson);
 
-//console.log(myJson);
+// console.log(myJson);
 // console.log(JSON.stringify(myJson[num].from));
-//console.log(JSON.stringify(myJson).length);
+// console.log(JSON.stringify(myJson).length);
 
-//}
-//}
+// }
+}
+
+getMantra()
